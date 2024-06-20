92b48e3dac3b9a0407afba932f2574e732de4705
Joshua Ross <josh@rossjm.dev>
2024-06-19 22:21:10 -0400

    Update portainer-install.sh

--- assets/scripts/portainer-install.sh
+++ assets/scripts/portainer-install.sh
@@ -10,7 +10,7 @@ HOST_IP=$(hostname -I | awk '{print $1}')
 docker volume create portainer_data
 
 # download and install the Portainer Server container
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+docker run -d -p 80:8000 -p 443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
 
 # verify portainer is running
 docker ps
