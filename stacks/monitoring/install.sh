2d514eb2509108328118d5b26478af6fc5a889db
Joshua Ross <josh@rossjm.dev>
2024-06-23 13:38:42 -0400

    added monitoring stack changed readme to verify changes.

---
+++ stacks/monitoring/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Author: Joshua Ross
+# Github: https://github.com/ColoredBytes
+# Purpose: Monitor Stack deployment
+
+# Create docker volumes
+  docker volume create influxdb-volume
+  docker volume create grafana-volume
+
+# Create Networks
+docker network create monitoring_network
