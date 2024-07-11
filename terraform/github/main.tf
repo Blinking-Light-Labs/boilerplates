0ce89202844438a33407c144ba8a3bfd056dea30
Joshua Ross <josh@rossjm.dev>
2024-07-11 09:07:17 -0400

    More changes on the gitthub boilerplate

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -1,12 +1,26 @@
 resource "github_repository" "example" {
-  name        = "example"
+  name        = var.repo_name
   description = "My awesome codebase"
 
   visibility = "public"
+}
 
-  template {
-    owner                = "github"
-    repository           = "terraform-template-module"
-    include_all_branches = true
-  }
+resource "github_repository_file" "example" {
+  repository          = var.repo_name
+  branch              = "main"
+  file                = "README.md"
+  content             = file("${path.module}/files/README.md")
+  commit_message      = "Init commit by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@example.com"
+  overwrite_on_create = true
+}
+
+module "repository" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
+
+  name               = var.repo_name
+  license_template   = "mit"
+  gitignore_template = "Terraform"
 }
---
+++ terraform/github/variables.tf
@@ -0,0 +1,12 @@
+variable "token" {
+    type = string
+    description = "GitHub PAT Token"
+    default = "value"
+}
+
+variable "repo_name" {
+    type = string
+    description = "Name of the repo"
+    
+  
+}
